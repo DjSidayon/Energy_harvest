cfdc459a44670a9a932b226af8a46011588b419e
DJ-LAPTOP\djsid <dj.sidayon@gmail.com>
2026-07-01 20:37:11 +0200

    Center aligned pwm for 90 deg phase shift

--- Src/gpio.c
+++ Src/gpio.c
@@ -12,19 +12,27 @@ void initGpio(void)
                           RCC_AHB2ENR_GPIOFEN |
                           RCC_AHB2ENR_GPIOGEN);
 
-    GPIOA->AFR[0] =       GPIO_AF2(6) |      // TIM3_CH1
-                          GPIO_AF2(7);       // TIM3_CH2
+    //GPIOA->AFR[0] =       GPIO_AF2(6) |      // TIM3_CH1
+    //                      GPIO_AF2(7);       // TIM3_CH2
 
     GPIOA->AFR[1] =       GPIO_AF0(13) |     // SWDIO
                           GPIO_AF0(14);      // SWCLK
+
+    GPIOB->AFR[0] =       GPIO_AF5(6);      // TIM8_CH1
+    GPIOB->AFR[1] =       GPIO_AF10(8);      // TIM8_CH2
+
 
     GPIOA->MODER =
             GPIO_ANALOG_MODE(0)     |   // ADC1_IN1
             GPIO_ANALOG_MODE(1)     |   // ADC1_IN2
             GPIO_ANALOG_MODE(2)     |   // ADC1_IN3
             GPIO_ANALOG_MODE(3)     |   // ADC1_IN4
-            GPIO_AF_MODE(6)         |   // TIM3_CH1
-            GPIO_AF_MODE(7)         |   // TIM3_CH2
+            //GPIO_AF_MODE(6)         |   // TIM3_CH1
+            //GPIO_AF_MODE(7)         |   // TIM3_CH2
             GPIO_AF_MODE(13)        |   // SWDIO
             GPIO_AF_MODE(14);           // SWCLK
+
+    GPIOB->MODER =
+            GPIO_AF_MODE(6) | //TIM8_CH1 (D10)
+            GPIO_AF_MODE(8); //TIM8_CH2 (D15)
 }
--- Src/pwm.c
+++ Src/pwm.c
@@ -2,12 +2,13 @@
 
 
 #define TIM_PSC             (100U - 1U)
-#define TIM_FREQ            ((double) 100.0)
+#define TIM_FREQ            ((double) 200e3)
 
-#define TIM_PWM_PERIOD      ROUND_TO(uint32_t, ((double) SYSTEM_CLOCK_HZ/(TIM_FREQ*(double)(TIM_PSC+1U)) ))
+//#define TIM_PWM_PERIOD      ROUND_TO(uint32_t, ((double) SYSTEM_CLOCK_HZ/(TIM_FREQ*(double)(TIM_PSC+1U)) ))
+#define TIM_PWM_PERIOD      ((uint32_t)((double) SYSTEM_CLOCK_HZ/(2*TIM_FREQ*(double)(TIM_PSC+1U))))
 
-#define TIM_PWM_DUTY_1     ROUND_TO(uint16_t , (double)TIM_PWM_PERIOD * 0.25)
-#define TIM_PWM_DUTY_2     ROUND_TO(uint16_t , (double)TIM_PWM_PERIOD * 0.75)
+#define TIM_PWM_DUTY_1     ROUND_TO(uint16_t , (double)TIM_PWM_PERIOD * 0.5)
+#define TIM_PWM_DUTY_2     ROUND_TO(uint16_t, TIM_PWM_DUTY_1 + (TIM_PWM_PERIOD >> 2)) //90° Verschiebung
 
 /* Function prototype */
 static void initTim3(void);
@@ -19,10 +20,12 @@ void initPwm(void)
 
 static void initTim3(void)
 {
-    TIM_TypeDef * const TIM_PWM = TIM3;
+    //TIM_TypeDef * const TIM_PWM = TIM3;
+    TIM_TypeDef * const TIM_PWM = TIM8;
 
     // enable timer clock
-    SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_TIM3EN);
+    //SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_TIM3EN);
+    SET_BIT(RCC->APB2ENR, RCC_APB2ENR_TIM8EN);
 
     TIM_PWM->PSC = TIM_PSC;       // prescale register
     // auto reload register value: 1000us / (1/system clock)
@@ -36,10 +39,18 @@ static void initTim3(void)
     TIM_PWM->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 |TIM_CCMR1_OC1PE |
                       TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1 |TIM_CCMR1_OC2PE;
 
+
+
     // auto-reload preload enable
-    TIM_PWM->CR1 = TIM_CR1_ARPE;
+    TIM_PWM->CR1 |= TIM_CR1_ARPE;
+
+    // Center-aligned mode 1 (CMS = 01) --> interrupt flags of output compare set when counting down.
+    TIM_PWM->CR1 |= TIM_CR1_CMS_0;
 
     TIM_PWM->EGR = TIM_EGR_UG;
+
+    /* Enable main output (MOE) — required for TIM1/TIM8 */
+    TIM_PWM->BDTR |= TIM_BDTR_MOE;
 
     // enable output on channel 1, 2
     SET_BIT(TIM_PWM->CCER, TIM_CCER_CC1E);
